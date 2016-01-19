b2d30b7cc166a3ffb631b0dac6292fb08fc1dd77
Todd <toddm92@gmail.com>
2016-01-18 20:48:12 -0800

    add profile argument and remove region prompting

--- subnet-test.py
+++ subnet-test.py
@@ -11,12 +11,7 @@ from __future__ import print_function
 
 import boto.vpc
 import boto.ec2
-import sys
-
-# ** Modify as needed **
-#
-PROFILE = 'eng'    # (from your ~/.boto)
-# **
+import sys, getopt
 
 # Dummy varibles
 #
@@ -29,9 +24,32 @@ def usage():
 
   print("""
 
-  Testing VPC subnet creation in all availabe zones..
+  Test VPC subnet creation in all availabe zones
+
+  vpc-subnet-test.py -p <profile>
 
   """)
+  exit(1)
+
+def get_args():
+  """ Check the arguments """
+
+  argv = sys.argv[1:]
+  profile = ''
+
+  try:
+    opts, args = getopt.getopt(argv,'hp:',['profile='])
+  except getopt.GetoptError:
+    usage()
+
+  for opt, arg in opts:
+    if opt in ('-p', '--profile'):  profile = arg
+    elif opt == '-h':               usage()
+
+  if profile == '' or len(sys.argv) != 3:
+    usage()
+
+  return profile
 
 def get_regions():
   """ Build a region list """
@@ -44,86 +62,72 @@ def get_regions():
 
   return reg_list
 
-# Print usage
-#
-usage()
-
-# Test each region (main loop)
-#
 regions = get_regions()
+profile = get_args()
 reg_total = len(regions)
 reg_no = 0
+
+print("Testing VPC subnet creation in all availabe zones..\n")
 while reg_total > 0:
+  # Test each region
+  #
+  reg_name = regions[reg_no]
+  myregion = boto.ec2.get_region(region_name=reg_name)
 
-    # Ask to test each region
-    #
-    reg_name = regions[reg_no]
-    print("Test region", reg_name, "[yes]? (enter 'q' to quit) ", end = "")
-    rsp = raw_input()
-    rsp = rsp.lower()
-    if rsp == "":
-        rsp = "yes"
+  try:
+    conn = boto.vpc.VPCConnection(profile_name=profile, region=myregion)
+  except boto.provider.ProfileNotFoundError as e:
+    print(e.message)
+    exit(1)
+  else:
+    print("Region:", reg_name)
 
-    if rsp in ( "q", "qu", "qui", "quit" ):
-        reg_total = 0
-        
-    elif rsp not in ( "y", "ye", "yes" ):
-        reg_no += 1; reg_total -= 1
+  # Create a test VPC
+  #
+  try:
+    test_vpc = conn.create_vpc(VPC_CIDR, instance_tenancy='default')
+  except boto.exception.EC2ResponseError as e:
+    print(e.message, "\n")
+    reg_no += 1; reg_total -= 1
+    continue
 
+  # Get all available zones
+  #
+  all_azs = conn.get_all_zones()
+  az_total = len(all_azs)
+
+  sub_list = [ ]
+  sub_total = 0
+  az_no = 0
+
+  # Test each AZ by attempting to create a subnet
+  #
+  while az_no < az_total:
+    az_name = str(all_azs[az_no])
+    az_name = az_name[5:]
+    print("Attempting", az_name, end = "")
+
+    try:
+      sub = conn.create_subnet(test_vpc.id, SUBNETS[sub_total], availability_zone=az_name)
+    except boto.exception.EC2ResponseError as e:
+      print(" ..failed!")
+      print(e.message, "\n")
+      az_no += 1
     else:
-        # Do the work
-        #
-        print("Region:", reg_name)
-        myregion = boto.ec2.get_region(region_name=reg_name)
-        conn = boto.vpc.VPCConnection(profile_name=PROFILE, region=myregion)
+      print(" ..success!")
+      sub_list.append(sub.id)
+      sub_total += 1; az_no += 1
 
-        # Create a test VPC
-        #
-        try:
-          test_vpc = conn.create_vpc(VPC_CIDR, instance_tenancy='default')
-        except boto.exception.EC2ResponseError as e:
-          print(e.message, "\n")
-          reg_no += 1; reg_total -= 1
-          continue
+  # Clean up the mess
+  #
+  print("\nCleaning up", end = "")
+  while sub_total != 0:
+    sub_total -= 1
+    conn.delete_subnet(sub_list[sub_total])
 
-        # Get all available zones
-        #
-        all_azs = conn.get_all_zones()
-        az_total = len(all_azs)
+  conn.delete_vpc(test_vpc.id)
+  print(" ..done!\n")
 
-        sub_list = [ ]
-        sub_total = 0
-        az_no = 0
-
-        # Test each AZ by attempting to create a subnet
-        #
-        while az_no < az_total:
-
-            az_name = str(all_azs[az_no])
-            az_name = az_name[5:]
-            try:
-                print("Attempting", az_name, end = "")
-                sub = conn.create_subnet(test_vpc.id, SUBNETS[sub_total], availability_zone=az_name)
-                print(" ..success!")
-                sub_list.append(sub.id)
-                sub_total += 1
-                az_no += 1
-            except boto.exception.EC2ResponseError as e:
-                print(" ..failed!")
-                print(e.message, "\n")
-                az_no += 1
-
-        # Clean up the mess
-        #
-        print("\nCleaning up", end = "")
-        while sub_total != 0:
-
-            sub_total -= 1
-            conn.delete_subnet(sub_list[sub_total])
-
-        conn.delete_vpc(test_vpc.id)
-        print(" ..done!\n")
-
-        reg_no += 1; reg_total -= 1
+  reg_no += 1; reg_total -= 1
 #
-# (end main loop)
+print("Testing complete.")
